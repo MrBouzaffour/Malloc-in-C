b09e872543706d1fb451fbcbd1979886d0f0d770
ahmed <ahmedbouzaffour2002@gmail.com>
2024-08-20 23:37:28 +0000

    Basic functionality of heap_alloc

--- main.c
+++ main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#define HEAP_CAPACITY 640000
 
-#define CAPACITY 640000
-
-char heap[CAPACITY] = {0};
+char heap[HEAP_CAPACITY] = {0};
 size_t heap_size = 0;
 
 void *heap_alloc(size_t size)
 {
-	return NULL;
+	assert(heap_size + size <= HEAP_CAPACITY);
+	void *result = heap + heap_size;
+	heap_size +=size;
+	return result;
 }
 
 void heap_free(void *ptr)
 {
+	assert(false && "TODO : heap_free is not implemented");
 
 }
 
 void heap_collect()
 {
-	
+	assert(false && "TODO : heap_collect is not implemented");
 }
 
 
